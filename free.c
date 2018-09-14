2cbb7b873805f18296900ed9e343d51ec77571b2
jahlela <jahlela5@gmail.com>
2018-09-14 02:34:39 +0200

    Assign next_free and next after new amount is passed in

--- free.c
+++ free.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 // int a = 1025;
 // int *p;
 // p = &a; // address of a
-// *p <- value at p, ie the value at address of a
-// &p <- address of p
+// *p is value at p, ie the value at address of a
+// &p is address of p
 
 
 // malloc
@@ -24,9 +25,9 @@
 // OUT: int error, where 0 is no error, and any other value could be mapped to errors
 // Splices an address in memory into the linked list of free spots
 // When a spot is freed:
-// 
-// 
-// 
+//
+//
+//
 // Tests:
 // - Is memory initialized, and the correct size
 // - Is next_free initialized to 0
@@ -35,16 +36,24 @@
 //   - Next times: return next_free, and set next_free += 2 += amount
 // - Can malloc be called with something too big?
 //   - Traverse from next_free to end and return 0 if nothing is large enough
-// - 
+// -
 
 
-// atoi: ascii to int, 
-
-int main(){
-    return 0;
-}
+// atoi: ascii to int,
 
 
+/*
+ *
+ *  virtual memory:
+ *     0 | noise | program source in assembly bytecode |  stack | constants  
+ *     0 | noise | program source in assembly bytecode |  stack | cons | memory = heap
+ *
+ *
+ *
+ *
+ */
+
+// Everything is an integer
 
 // FREE_T
 typedef struct free_block {
@@ -52,38 +61,64 @@ typedef struct free_block {
     size_t size;
 } free_t;
 
+
 #define MEM_SIZE 1024
 
-// INITIALIZE not_called, memory, next_free
-int not_called = 1;
+// Define memory
 char memory[MEM_SIZE];
-free_t* next_free = (free_t *)memory;
 
+// Define and initialize not_called and next_free
+int not_called = 1;
+free_t * next_free = (free_t *)memory;
 
 // MY_MALLOC
-char* my_malloc(size_t amount) {
+char * my_malloc(size_t amount) {
     // If first call, set first values of memory to 0 and the remaining free memory
     if (not_called) {
         next_free->next = 0;
         next_free->size = MEM_SIZE - sizeof(free_t);
         not_called = 0;
+        int new_free = (int)next_free;
+        printf("new_free: %i\n",new_free);
+        printf("*memory: %d\n", *memory);
+        printf("memory: %p\n", memory);
+        char * memory1 = memory;
+        printf("memory1: %p\n", memory1);
+        int  * memory2 = (int *)memory;
+        printf("memory2: %p\n", memory2);        
+        
+        
+        *(free_t**)memory = next_free;
+        // free_t is a custom-defined struct, which is a type
+        // free_t* is a pointer to a free_t
+        // free_t** is a pointer to a pointer to a free_t
+        // *(free_t**) 
+        next_free->next = (free_t *)(&memory[amount]);
     }
 
-//    printf("next: %d\n",next_free->next);
-//    printf("size: %d\n",next_free->size);    
+    printf("next: %p\n", next_free->next);
+    printf("size: %p\n", (void *)next_free->size); // printf("size: %p\n",(void *)next_free->size); // (void *)next_free->size);
+    printf("amount: %zu\n", amount); // printf("amount: %p\n",(void *)amount->size);
+
+
 
     // If size not enough for amount + 2, return 0
-    if ( next_free->size < (amount + sizeof(free_t))) {
+    size_t to_allocate = amount + sizeof(free_t);
+    printf("to_allocate: %zu\n",to_allocate);
+    if ( next_free->size < to_allocate) {
         return 0;
     } else {
         // set next_free->next to next_free->next + amount
         // return the address of the old next_free->next
-//        next_free->next = 
+        // next_free->next =
         return (char*)next_free;
     }
-    
+
     return (char *)next_free;
+
 }
+
+
 
 
 // MY_FREE
@@ -94,3 +129,26 @@ int my_free(char* address) {
     }
     return 1;
 }
+
+
+int main(){
+    char* new_var_pointer = my_malloc(5);
+    printf("new_var_pointer: %p\n", new_var_pointer);
+    return 0;
+}
+
+
+    //
+    // printf("next: %p\n", next_free->next);
+    // printf("size: %p\n", (void *)next_free->size); // printf("size: %p\n",(void *)next_free->size); // (void *)next_free->size);
+    // printf("amount: %zu\n", amount); // printf("amount: %p\n",(void *)amount->size);
+
+
+
+
+/*
+        char str[] = "November";
+        printf("Length of String is %d\n", strlen(str));
+        printf("mem of String is %d\n", strlen(str) * sizeof(str);
+        printf("Size of String is %lu\n", sizeof(str));
+*/
