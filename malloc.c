f751a9b6dd66e80bd0156ba5fc01acebed8448c9
jahlela <jahlela5@gmail.com>
2018-11-19 02:14:07 +0100

    Add some more assertions

--- malloc.c
+++ malloc.c
@@ -24,12 +24,12 @@ char* my_malloc(int amount) {
     not_called = 0;
   }
 
-  printf("Amount requested: %d\n", amount);        
+  printf("Amount requested: %d\n", amount);
   // Calculate necessary space for newly allocated memory
   size_t to_allocate = amount + sizeof(node_t);
   printf("Size of node_t: %zu\n", sizeof(node_t));
   printf("Memory to allocate: %zu\n", to_allocate);
-  printf("Remaining space: %zu\n", next_free->size);        
+  printf("Remaining space: %zu\n", next_free->size);
   // If size not enough for amount + sizeof(node_t), return 0
   if (next_free->size < to_allocate) {
     return 0;
@@ -52,8 +52,8 @@ char* my_malloc(int amount) {
 int my_free(char* address) {
     printf("address %p\n", address);
     // set to_dealloc to next_free
-    node_t* to_dealloc = next_free;    
     // set next_free to address
+    node_t* to_dealloc = next_free;    
     next_free = (node_t*)address;
 
     assert(next_free == (node_t*)address);
@@ -62,31 +62,54 @@ int my_free(char* address) {
 
 
 int test_malloc() {
+    int admin_size = sizeof(node_t);
+
     printf("Starting to run malloc: \n");
     printf("memory: %p\n", memory);
-    printf("next_free: %p\n", next_free);    
+    printf("next_free: %p\n", next_free);
 
+    // Test that after allocating 5, next_free is new_var_pointer05 + admin_size + 5
     node_t* new_var_pointer05 = (node_t*)my_malloc(5);
-    printf("new_var_pointer05: %p\n", new_var_pointer05);    
+    printf("new_var_pointer05: %p\n", new_var_pointer05);
+    assert(next_free == new_var_pointer05 + admin_size + 5);
 
+    // Test that after freeing new_var_pointer05, next_free is new_var_pointer05
     printf("\nStarting to free new_var_pointer05: \n");
-    printf("next_free: %p\n", next_free);    
+    printf("next_free: %p\n", next_free);
     my_free((char*)new_var_pointer05);
     printf("next_free: %p\n", next_free);
+    assert(next_free == new_var_pointer05);
 
+    // Test that after allocating new_var_pointer5, it is equal to old new_var_pointer05
     node_t* new_var_pointer5 = (node_t*)my_malloc(5);
-    printf("new_var_pointer5: %p\n", new_var_pointer5);    
+    printf("new_var_pointer5: %p\n", new_var_pointer5);
+    assert(new_var_pointer5 == new_var_pointer05);
 
+    // Test that after allocating 5, next_free is next_free + admin_size + 10
     node_t* new_var_pointer10 = (node_t*)my_malloc(10);
     printf("new_var_pointer10: %p\n", new_var_pointer10);
+    assert(next_free = new_var_pointer10 + admin_size + 10);
 
+    // Test that after deallocating new_var_pointer10, next_free does not change
+    node_t* old_next_free = next_free;
     printf("\nStarting to free new_var_pointer10: \n");
-    printf("next_free: %p\n", next_free);    
+    printf("next_free: %p\n", next_free);
+    my_free((char*)new_var_pointer10);
+    printf("old_next_free: %p\n", old_next_free);
+    printf("next_free: %p\n", next_free);
+    assert(next_free == old_next_free);
+
+    // Test that if the requested amount exceeds the remaining space, it returns 0
+    printf("\nStarting to free new_var_pointer10: \n");
+    printf("next_free: %p\n", next_free);
     my_free((char*)new_var_pointer10);
     printf("next_free: %p\n", next_free);
-    
+
     node_t* new_var_pointer1020 = (node_t*)my_malloc(1020);
     printf("new_var_pointer1020: %p\n", new_var_pointer1020);
+
+
+    // Allocate something big, then something small, then deallocate
 
     return 1;
 }
